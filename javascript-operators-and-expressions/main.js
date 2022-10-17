8c5fb48d94186a45965838f494a171698e68b989
“Louis <Louisgeemod@gmail.com>
2022-10-17 18:42:03 +0000

    completed javascript-operators-and-expressions exercise for Louis Zhang

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 15;
+var height = 26;
+var area = width * height;
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+
+var bill = 45;
+var payment = 60;
+var change = payment - bill;
+console.log('value of change:', change);
+console.log('typeof change:', typeof change);
+
+var quizzes = 100;
+var midterm = 91;
+var final = 57;
+var grade = (quizzes + midterm + final) / 3;
+console.log('value of grade:', grade);
+console.log('typeof log:', typeof grade);
+
+var firstName = 'Louis';
+var lastName = 'Zhang';
+var fullName = firstName + ' ' + lastName;
+console.log('value of fullName:', fullName);
+console.log('type of fullName:', typeof fullName);
+
+var pH = 16;
+var isAcidic = pH < 7;
+console.log('value of isAcidic:', isAcidic);
+console.log('type of isAcidic:', typeof isAcidic);
+
+var headCount = 35;
+var isSparta = headCount === 300;
+console.log('value of isSparta:', isSparta);
+console.log('type of isSparta:', typeof isSparta);
+
+var motto = fullName;
+motto = fullName + ' is the GOAT';
+console.log('value of motto:', motto);
+console.log('typeof Motto:', typeof motto);
