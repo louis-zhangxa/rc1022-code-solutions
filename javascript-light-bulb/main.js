75ec6e7dcf682575e6abc453c24296cb782cc1fd
“Louis <Louisgeemod@gmail.com>
2022-10-25 19:13:34 +0000

    complete 4 javascript-light-bulb

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,17 +1,20 @@
-var clicked = 0;
+/* grab element from dom
+make a funtion to see if user clicked the button. if clicked, turn on. click again, turn off
+add eventlistner for moniter user input */
+var lightOff = true;
 
 var $switch = document.querySelector('.button');
 var $backGround = document.querySelector('.background');
 
 function lightBulb(event) {
-  clicked++;
-  var lightOff = (clicked % 2 === 0);
-  if (lightOff === true) {
+  if (lightOff === false) {
     $switch.className = 'button';
     $backGround.className = 'background';
+    lightOff = true;
   } else {
     $switch.className = 'button on';
     $backGround.className = 'background on';
+    lightOff = false;
   }
 }
 
