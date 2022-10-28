3fb90d689b2285196eab39e51cb6751214c540b4
“Louis <Louisgeemod@gmail.com>
2022-10-28 20:32:42 +0000

    javascript-local-storage

---
+++ javascript-local-storage/data.js
@@ -0,0 +1,14 @@
+/* exported todos */
+
+var todos = [];
+var previousTodosJSON = localStorage.getItem('javascript-local-storage');
+if (previousTodosJSON !== null) {
+  todos = JSON.parse(previousTodosJSON);
+}
+
+window.addEventListener('beforeunload', string);
+
+function string(event) {
+  var todosJSON = JSON.stringify(todos);
+  localStorage.setItem('javascript-local-storage', todosJSON);
+}
