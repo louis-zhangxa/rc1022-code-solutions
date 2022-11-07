654a4edf5d89f79ed708875cb1aaffcc253c5e2e
“Louis <Louisgeemod@gmail.com>
2022-11-07 05:07:44 +0000

    array-manipulation-2

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,11 +1,19 @@
 /* exported chunk */
-// function chunk(array, size) {
-//   var output = [];
-//   for (var j = 0; j < array.length; j++) {
-//     for (var i = 0; i < size; i++) {
-//       var ea = [];
-//       ea += array[i];
-//       output += ea;
-//     }
-//   }
-// }
+function chunk(array, size) {
+  var output = [];
+  var list = [];
+  var count = 0;
+  for (var j = 0; j < array.length; j++) {
+    list.push(array[j]);
+    count++;
+    if (count === size) {
+      output.push(list);
+      count = 0;
+      list = [];
+    }
+  }
+  if (count / array.length !== 0 && list.length !== 0) {
+    output.push(list);
+  }
+  return output;
+}
