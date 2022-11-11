2f7b6239339572be78a7ed828d281c052a53abea
“Louis <Louisgeemod@gmail.com>
2022-11-11 00:44:39 +0000

    finished object-manipulation-2

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -2,20 +2,14 @@
 function omit(source, keys) {
   var output = {};
   var list = Object.keys(source);
-  for (var i = 0; i < list.length; i++) {
-    if (keys[i] !== list[i]) {
-      output[keys[i]] = source[keys[i]];
+  for (var i = 0; i < keys.length; i++) {
+    for (var j = 0; j < list.length; j++) {
+      if (keys[j] === list[i]) {
+        break;
+      } else {
+        output[list[i]] = source[list[i]];
+      }
     }
   }
   return output;
 }
-
-// function pick(source, keys) {
-//   var output = {};
-//   for (var i = 0; i < keys.length; i++) {
-//     if (source[keys[i]] !== undefined) {
-//       output[keys[i]] = source[keys[i]];
-//     }
-//   }
-//   return output;
-// }
