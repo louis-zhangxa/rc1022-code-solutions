047bb983405d625e1e2424973d49bce97c1f5eb2
“Louis <Louisgeemod@gmail.com>
2022-11-30 00:29:41 +0000

    node-module-system

---
+++ node-module-system/a-wrapped-module.js
@@ -0,0 +1,5 @@
+console.log('type of exports is: "', typeof exports, '" ,its value is: "', exports, '"');
+console.log('type of require is: "', typeof require, '" ,its value is: "', require, '"');
+console.log('type of module is: "', typeof module, '" ,its value is: "', module, '"');
+console.log('type of __filename is: "', typeof __filename, '" ,its value is: "', __filename, '"');
+console.log('type of __dirname is: "', typeof __dirname, '" ,its value is: "', __dirname, '"');
