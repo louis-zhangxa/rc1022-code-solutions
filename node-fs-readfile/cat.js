810aba5882b4a35ef6ad0d3fbc9ef9c5de6fc033
“Louis <Louisgeemod@gmail.com>
2022-12-01 00:10:36 +0000

    inprogress level 3

---
+++ node-fs-readfile/cat.js
@@ -0,0 +1,27 @@
+// const fs = require('fs');
+
+// // for (var i = 2; i < process.argv.length; i++) {
+// //   fs.readFile(process.argv[i], 'utf-8', (err, data) => {
+// //     if (err) throw err;
+// //     console.log(data);
+// //   });
+// // }
+// fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+// fs.readFile(process.argv[3], 'utf-8', (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+// fs.readFile(process.argv[4], 'utf-8', (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+// fs.readFile(process.argv[5], 'utf-8', (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
