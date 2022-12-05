b88bf69201c2c11e1faf8e43fe0bdf05f59e6d92
“Louis <Louisgeemod@gmail.com>
2022-12-05 19:41:13 +0000

    finished express-post-json

---
+++ express-post-json/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+let nextID = 1;
+const grades = {};
+
+app.get('/api/grades', (req, res) => {
+  const keys = [];
+  for (const key in grades) {
+    keys.push(grades[key]);
+  }
+  res.json(keys);
+});
+
+app.use(express.json());
+
+app.post('/api/grades', (req, res) => {
+  res.status(201);
+  req.body.ID = nextID++;
+  grades[req.body.ID] = req.body;
+  res.json(req.body);
+});
+
+app.listen(3000, (req, res) => {
+  // eslint-disable-next-line no-console
+  console.log('server is aliveeeee!!!!!');
+});
