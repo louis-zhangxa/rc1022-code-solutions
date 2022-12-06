8975c2b19b45f1a513f69214b333eddffc58ec88
“Louis <Louisgeemod@gmail.com>
2022-12-06 18:16:05 +0000

    unfinished express-notes-api

---
+++ express-notes-api/index.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const json = require('./data.json');
+const notes = json.notes;
+
+const write = () => {
+  fs.writeFile('./data.json', JSON.stringify(json, null, 2), 'utf-8', (err, data) => {
+    if (err) throw err;
+  });
+};
+
+app.get('/api/notes', (req, res) => {
+  const keys = [];
+  for (const key in notes) {
+    keys.push(notes[key]);
+  }
+  res.json(keys);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  if (req.params.id < 0) {
+    const erroeMessage = { error: "No id is negetive you stupid, don't mess around with this server!!!!!" };
+    res.status(400).json(erroeMessage);
+  } else {
+    if (!notes[req.params.id]) {
+      const erroeMessage = { error: "No id is match the object, don't be a fool okay?" };
+      res.status(404).json(erroeMessage);
+    } else {
+      res.json(notes[req.params.id]);
+    }
+  }
+});
+
+app.use(express.json());
+
+app.post('/api/notes', (req, res) => {
+  if (!req.body.content) {
+    const erroeMessage = { error: 'PUT SOMETHING PLEASE!!!!' };
+    res.status(400).json(erroeMessage);
+  } else {
+    req.body.id = json.nextId;
+    notes[json.nextId] = req.body;
+    json.nextId++;
+    write();
+    res.status(201).json(req.body);
+  }
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  if (req.params.id < 0) {
+    const erroeMessage = { error: "No id is negetive you stupid, don't mess around with this server!!!!!" };
+    res.status(400).json(erroeMessage);
+  } else {
+    if (!notes[req.params.id]) {
+      const erroeMessage = { error: "No id is match the object, don't be a fool okay?" };
+      res.status(404).json(erroeMessage);
+    } else {
+      delete notes[req.params.id];
+      write();
+      res.sendStatus(204);
+    }
+  }
+});
+
+app.put('/api/notes/:id', (req, res) => {
+  if (req.params.id < 0) {
+    const erroeMessage = { error: "No id is negetive you stupid, don't mess around with this server!!!!!" };
+    res.status(400).json(erroeMessage);
+  } else {
+    if (!notes[req.params.id]) {
+      const erroeMessage = { error: "No id is match the object, don't be a fool okay?" };
+      res.status(404).json(erroeMessage);
+    } else {
+      req.body.id = req.params.id;
+      notes[req.params.id] = req.body;
+      json.nextId++;
+      write();
+      res.sendStatus(200);
+    }
+  }
+});
+
+app.listen(3000, () => {
+  // eslint-disable-next-line no-console
+  console.log('server is aaaaallllllliiiiiivvvveeeee!!!!!');
+});
