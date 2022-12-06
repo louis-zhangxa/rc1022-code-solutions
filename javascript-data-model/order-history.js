9e13fba95357cd14b2b6335ae873643926b34637
“Louis <Louisgeemod@gmail.com>
2022-12-06 22:23:57 +0000

    final for javascript-data-model

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -1,99 +1,107 @@
 var orderHistory = [
-  [
-    {
+  {
+    orderInfo: {
       OrderNumber: '114 - 3941689 - 8772232',
       FinalPrice: 34.00,
       shipTo: 'JS Masher'
     },
-    {
-      item: 'JavaScript for impatient programmers',
-      author: 'Rauschmayer, Dr. Axel',
-      returnWindow: 'sep, 7, 2020',
-      deliveryDate: 'august, 8, 2020',
-      orderPlacedDate: 'august, 4, 2020',
-      delivered: true,
-      priceOfTheItem: 31.55,
-      eligibleForReturn: true,
-      returned: false,
-      productSupport: false,
-      productReview: null,
-      deliveryToResident: true
-    }
-  ],
-  [
-    {
+    orders: [
+      {
+        item: 'JavaScript for impatient programmers',
+        author: 'Rauschmayer, Dr. Axel',
+        returnWindow: 'sep, 7, 2020',
+        deliveryDate: 'august, 8, 2020',
+        orderPlacedDate: 'august, 4, 2020',
+        delivered: true,
+        priceOfTheItem: 31.55,
+        eligibleForReturn: true,
+        returned: false,
+        productSupport: false,
+        productReview: null,
+        deliveryToResident: true
+      }
+    ]
+  },
+  {
+    orderInfo: {
       OrderNumber: '113-9984268-1280257',
       FinalPrice: 44.53,
       shipTo: 'JS Masher'
     },
-    {
-      item: 'The Timeless Way of Building',
-      author: 'Alexander, Christopher',
-      returnWindow: 'aug, 19, 2020',
-      deliveryDate: 'July, 20, 2020',
-      orderPlacedDate: 'July, 19, 2020',
-      priceOfTheItem: 41.33,
-      delivered: true,
-      eligibleForReturn: true,
-      returned: false,
-      productSupport: false,
-      productReview: null,
-      deliveryToResident: true
-    }
-  ],
-  [
-    {
+    orders: [
+      {
+        item: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        returnWindow: 'aug, 19, 2020',
+        deliveryDate: 'July, 20, 2020',
+        orderPlacedDate: 'July, 19, 2020',
+        priceOfTheItem: 41.33,
+        delivered: true,
+        eligibleForReturn: true,
+        returned: false,
+        productSupport: false,
+        productReview: null,
+        deliveryToResident: true
+      }
+    ]
+  },
+  {
+    orderInfo: {
       OrderNumber: '114-2875557-9059409',
       FinalPrice: 17.22,
       shipTo: 'JS Masher'
     },
-    {
-      item: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for Wii U, PC.Support Turbo and Vibration Features.No Driver and No Lag- Gamecube Adapter',
-      author: null,
-      returnWindow: 'aug, 5, 2020',
-      deliveryDate: 'July, 7, 2020',
-      orderPlacedDate: 'July, 4, 2020',
-      priceOfTheItem: 15.98,
-      delivered: true,
-      eligibleForReturn: false,
-      returned: false,
-      productSupport: false,
-      productReview: null,
-      deliveryToResident: true
-    }
-  ],
-  [
-    {
+    orders: [
+      {
+        item: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for Wii U, PC.Support Turbo and Vibration Features.No Driver and No Lag- Gamecube Adapter',
+        author: null,
+        returnWindow: 'aug, 5, 2020',
+        deliveryDate: 'July, 7, 2020',
+        orderPlacedDate: 'July, 4, 2020',
+        priceOfTheItem: 15.98,
+        delivered: true,
+        eligibleForReturn: false,
+        returned: false,
+        productSupport: false,
+        productReview: null,
+        deliveryToResident: true
+      }
+    ]
+  },
+  {
+    orderInfo: {
       OrderNumber: '113-2883177-2648248',
       FinalPrice: 138.93,
       shipTo: 'JS Masher'
     },
-    {
-      item: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
-      author: null,
-      returnWindow: 'aug, 4, 2020',
-      priceOfTheItem: 94.95,
-      deliveryDate: 'July, 5, 2020',
-      orderPlacedDate: 'July, 3, 2020',
-      delivered: true,
-      eligibleForReturn: false,
-      returned: false,
-      productSupport: true,
-      productReview: null,
-      deliveryToResident: false
-    },
-    {
-      item: 'The Art of Sql',
-      author: 'Faroult, Stephane',
-      returnWindow: 'aug, 4, 2020',
-      priceOfTheItem: 33.99,
-      deliveryDate: 'July, 5, 2020',
-      orderPlacedDate: 'July, 3, 2020',
-      delivered: true,
-      eligibleForReturn: false,
-      returned: false,
-      productSupport: true,
-      deliveryToResident: false
-    }
-  ]
+    orders: [
+      {
+        item: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        author: null,
+        returnWindow: 'aug, 4, 2020',
+        priceOfTheItem: 94.95,
+        deliveryDate: 'July, 5, 2020',
+        orderPlacedDate: 'July, 3, 2020',
+        delivered: true,
+        eligibleForReturn: false,
+        returned: false,
+        productSupport: true,
+        productReview: null,
+        deliveryToResident: false
+      },
+      {
+        item: 'The Art of Sql',
+        author: 'Faroult, Stephane',
+        returnWindow: 'aug, 4, 2020',
+        priceOfTheItem: 33.99,
+        deliveryDate: 'July, 5, 2020',
+        orderPlacedDate: 'July, 3, 2020',
+        delivered: true,
+        eligibleForReturn: false,
+        returned: false,
+        productSupport: true,
+        deliveryToResident: false
+      }
+    ]
+  }
 ];
