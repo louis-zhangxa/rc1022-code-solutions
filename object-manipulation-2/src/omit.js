f9a474836c231b6fde4a228207ee53c0802321c3
“Louis <Louisgeemod@gmail.com>
2022-12-09 19:53:33 +0000

    inprogress object-manipulation-2

--- object-manipulation-2/src/omit.js
+++ object-manipulation-2/src/omit.js
@@ -5,25 +5,34 @@ loop through all the key in source object
 loop through all the item in keys array to see if they exist in source
 if exist, next key
 if not, add to output */
-function omit(source, keys) {
-  var output = {};
-  for (var i = 0; i < keys.length; i++) {
-    if (source[keys[i]] === undefined) {
-      output[keys[i]] = source[keys[i]];
-    }
-  }
-  return output;
-}
-
-// var output = {};
-// var list = Object.keys(source);
-// for (var i = 0; i < keys.length; i++) {
-//   for (var j = 0; j < list.length; j++) {
-//     if (keys[j] === list[i]) {
-//       break;
-//     } else {
-//       output[list[i]] = source[list[i]];
+// function omit(source, keys) {
+//   var output = {};
+//   for (var i = 0; i < keys.length; i++) {
+//     for (var key in source) {
+//       console.log(keys[i]);
+//       console.log(key);
+//       if (keys[i] === key) {
+//         break;
+//       } else {
+//         output[key] = source[key];
+//       }
 //     }
 //   }
+//   console.log(output);
+//   return output;
 // }
-// return output;
+
+// function omit(source, keys) {
+//   var output = {};
+//   var list = Object.keys(source);
+//   for (var i = 0; i < keys.length; i++) {
+//     for (var j = 0; j < list.length; j++) {
+//       if (keys[j] === list[i]) {
+//         break;
+//       } else {
+//         output[list[i]] = source[list[i]];
+//       }
+//     }
+//   }
+//   return output;
+// }
