a960f34d2b8f1ebdd8990b7f07d93dca0cfd23d8
“Louis <Louisgeemod@gmail.com>
2022-12-09 22:55:20 +0000

    array-map

--- array-map/map.js
+++ array-map/map.js
@@ -1,6 +1,6 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-function map(array, transform) {
+function mapse(array, transform) {
   const output = [];
   for (let i = 0; i < array.length; i++) {
     output.push(transform(array[i]));
@@ -16,8 +16,16 @@ function priceEach(num) {
   return `$${num.toFixed(2)}`;
 }
 
-const doubled = map(numbers, double);
+const doubled = mapse(numbers, double);
 console.log(doubled);
 
-const prices = map(numbers, priceEach);
+const prices = mapse(numbers, priceEach);
 console.log(prices);
+
+// const doubled = numbers.mapse(num => num * 2);
+// console.log(doubled);
+
+// const prices = numbers.mapse(num => {
+//   return `$${num.toFixed(2)}`;
+// });
+// console.log(prices);
