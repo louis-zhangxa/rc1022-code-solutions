2f9121d843c827326c0795cdebe9416fdd03e89b
Louis Zhang <Louisgeemod@gmail.com>
2022-12-17 00:48:21 +0000

    :finished react-stopwatch-component

--- react-stopwatch-component/src/stopwatch.jsx
+++ react-stopwatch-component/src/stopwatch.jsx
@@ -4,53 +4,34 @@ class Stopwatch extends React.Component {
   constructor(props) {
     super(props);
     this.props = props;
-    this.props.time = 5;
-    this.state = { class: 'fa-solid fa-play fa-4x' };
+    this.state = { time: 0, class: 'fa-solid fa-play fa-4x' };
     this.handleClick = this.handleClick.bind(this);
     this.reset = this.reset.bind(this);
   }
 
   handleClick() {
     if (this.state.class === 'fa-solid fa-play fa-4x') {
+      this.setState({ class: 'fa-solid fa-pause fa-4x' });
       this.countdown = setInterval(() => {
-        this.props.time--;
-        // console.log(this.props.time);
-        if (this.props.time === 0) {
-          clearInterval(this.countdown);
-          this.props.time = 5;
-          this.setState({ class: 'fa-solid fa-play fa-4x' });
-        }
+        this.setState({ time: this.state.time + 1 });
       }, 1000);
     } else if (this.state.class === 'fa-solid fa-pause fa-4x') {
-      clearInterval(this.countdown);
-      // console.log(this.props.time);
-    }
-
-    if (this.state.class === 'fa-solid fa-play fa-4x') {
-      this.setState({ class: 'fa-solid fa-pause fa-4x' });
-    } else if (this.state.class === 'fa-solid fa-pause fa-4x') {
       this.setState({ class: 'fa-solid fa-play fa-4x' });
+      clearInterval(this.countdown);
     }
   }
 
   reset() {
     clearInterval(this.countdown);
-    this.props.time = 5;
+    this.setState({ time: 0 });
     this.setState({ class: 'fa-solid fa-play fa-4x' });
   }
-
-  // componentDidUpdate(prevProps) {
-  //   console.log(prevProps.time);
-  //   if (this.props.time !== prevProps.time) {
-  //     this.fetchData(this.props.time);
-  //   }
-  // }
 
   render() {
     return (
       <div>
         <div onClick={this.reset} className="round">
-          <h1>{this.props.time}</h1>
+          <h1>{this.state.time}</h1>
         </div>
         <div className="text">
           <i onClick={this.handleClick} className={this.state.class}></i>
@@ -59,5 +40,12 @@ class Stopwatch extends React.Component {
     );
   }
 }
+
+// componentDidUpdate(prevProps) {
+//   console.log(prevProps.time);
+//   if (this.props.time !== prevProps.time) {
+//     this.fetchData(this.props.time);
+//   }
+// }
 
 export default Stopwatch;
